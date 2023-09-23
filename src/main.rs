bc20d8c5bfcc78b1c2e495d6d6e8eeec16466eac
do7be <bomsap700@gmail.com>
2023-09-23 22:20:56 +0900

    キャラを動かせるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,137 @@
 use bevy::prelude::*;
 
-fn main() {
-    App::new().add_systems(Update, hello_world_system).run();
+const PLAYER_SPEED: f32 = 500.0;
+
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
+
+#[derive(Component)]
+enum Direction {
+    Up,
+    Down,
 }
 
-fn hello_world_system() {
-    println!("hello world");
+#[derive(Component)]
+struct AnimationIndices {
+    first: usize,
+    last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Player
+    let texture_handle = asset_server.load("images/char.png");
+    let texture_atlas =
+        TextureAtlas::from_grid(texture_handle, Vec2::new(32.0, 32.0), 5, 1, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    let animation_indices = AnimationIndices { first: 2, last: 3 };
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(animation_indices.first),
+            transform: Transform::from_xyz(100., 0., 0.),
+            ..default()
+        },
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(0.33, TimerMode::Repeating)),
+        Direction::Up,
+        Player,
+    ));
+}
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(
+        &AnimationIndices,
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+    )>,
+) {
+    for (indices, mut timer, mut sprite) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            sprite.index = if sprite.index == indices.last {
+                indices.first
+            } else {
+                sprite.index + 1
+            };
+        }
+    }
+}
+
+fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
+    for (mut player, mut transform) in &mut sprite_position {
+        match *player {
+            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
+            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
+        }
+
+        if transform.translation.y > 200. {
+            *player = Direction::Down;
+        } else if transform.translation.y < -200. {
+            *player = Direction::Up;
+        }
+    }
+}
+
+#[derive(Component)]
+struct Player;
+
+fn move_player(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
+    time_step: Res<FixedTime>,
+) {
+    let mut player_transform = query.single_mut();
+    let mut direction = 0.0;
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        direction -= 1.0;
+        player_transform.scale.x = -1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::Right) {
+        direction += 1.0;
+        player_transform.scale.x = 1.0;
+    }
+
+    let new_player_position =
+        player_transform.translation.x + direction * PLAYER_SPEED * time_step.period.as_secs_f32();
+
+    player_transform.translation.x = new_player_position;
+}
+
+fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_step: Res<FixedTime>) {
+    for (mut transform, velocity) in &mut query {
+        transform.translation.x += velocity.x * time_step.period.as_secs_f32();
+        transform.translation.y += velocity.y * time_step.period.as_secs_f32();
+    }
+}
+
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup)
+            .add_systems(Update, animate_sprite)
+            .add_systems(Update, sprite_movement)
+            .add_systems(FixedUpdate, move_player.after(apply_velocity));
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(ImagePlugin::default_nearest()),
+            GamePlugin,
+        ))
+        .run();
 }
