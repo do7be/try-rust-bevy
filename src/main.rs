02f2b57976d10575a6f2c00e3e812d5ef762ff25
do7be <bomsap700@gmail.com>
2023-09-24 21:37:31 +0900

    ジャンプして落下するように

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 
 const PLAYER_SPEED: f32 = 500.0;
+const PLAYER_JUMP_FORCE: f32 = 44.0;
+const GRAVITY: f32 = 9.81 * 100.0;
 
 #[derive(Component, Deref, DerefMut)]
 struct Velocity(Vec2);
@@ -42,8 +44,11 @@ fn setup(
         },
         animation_indices,
         AnimationTimer(Timer::from_seconds(0.33, TimerMode::Repeating)),
-        Direction::Up,
-        Player,
+        Player {
+            direction: Direction::Down,
+            grounded: true,
+        },
+        Velocity(Vec2::new(0.0, 0.0)),
     ));
 }
 
@@ -67,46 +72,44 @@ fn animate_sprite(
     }
 }
 
-fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
-    for (mut player, mut transform) in &mut sprite_position {
-        match *player {
-            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
-            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
-        }
-
-        if transform.translation.y > 200. {
-            *player = Direction::Down;
-        } else if transform.translation.y < -200. {
-            *player = Direction::Up;
-        }
-    }
-}
-
 #[derive(Component)]
-struct Player;
+struct Player {
+    direction: Direction,
+    grounded: bool,
+}
 
 fn move_player(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<&mut Transform, With<Player>>,
+    mut query: Query<(&mut Player, &mut Transform, &mut Velocity)>,
     time_step: Res<FixedTime>,
 ) {
-    let mut player_transform = query.single_mut();
-    let mut direction = 0.0;
+    for (mut player, mut transform, mut velocity) in &mut query {
+        // Walk
+        let mut direction = 0.0;
 
-    if keyboard_input.pressed(KeyCode::Left) {
-        direction -= 1.0;
-        player_transform.scale.x = -1.0;
+        if keyboard_input.pressed(KeyCode::Left) {
+            direction = -1.0;
+            transform.scale.x = -1.0;
+        }
+
+        if keyboard_input.pressed(KeyCode::Right) {
+            direction = 1.0;
+            transform.scale.x = 1.0;
+        }
+
+        let new_player_position =
+            transform.translation.x + direction * PLAYER_SPEED * time_step.period.as_secs_f32();
+
+        transform.translation.x = new_player_position;
+
+        // Jump
+        if !player.grounded {
+            velocity.y -= GRAVITY * time_step.period.as_secs_f32();
+        } else if keyboard_input.pressed(KeyCode::Space) {
+            player.grounded = false;
+            velocity.y = PLAYER_JUMP_FORCE * 10.0; // ?
+        }
     }
-
-    if keyboard_input.pressed(KeyCode::Right) {
-        direction += 1.0;
-        player_transform.scale.x = 1.0;
-    }
-
-    let new_player_position =
-        player_transform.translation.x + direction * PLAYER_SPEED * time_step.period.as_secs_f32();
-
-    player_transform.translation.x = new_player_position;
 }
 
 fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_step: Res<FixedTime>) {
@@ -120,10 +123,13 @@ pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup)
+        app.insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
+            .add_systems(Startup, setup)
             .add_systems(Update, animate_sprite)
-            .add_systems(Update, sprite_movement)
-            .add_systems(FixedUpdate, move_player.after(apply_velocity));
+            .add_systems(
+                FixedUpdate,
+                (move_player.before(apply_velocity), apply_velocity),
+            );
     }
 }
 
